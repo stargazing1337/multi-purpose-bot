45f1de41e7bd4dbf1260cf09217c5ddf8d3a402c
Glitch (hello-express) <none>
2019-02-11 22:24:44 +0000

    👛🖖 Checkpoint ./server.js:3080416/482

--- server.js
+++ server.js
@@ -14,4 +14,16 @@ client.on('ready',() => { //when bot is ready
 client.on('message', (message) => { // when message is recieved
   
   if(message.author.bot) return //If the author is a bot exit code
-  if (!message.content.startsWith(prefix)) return; // 
+  if (!message.content.startsWith(prefix)) return; // If the message doesnt start with the prefix exit code
+  
+  const args = message.content.slice(prefix.length).trim().split(/ +/g); //define args
+  const command = args.shift.toLowerCase(); // define command
+  
+  if(command === "ping") { //Command ping
+    message.channel.send('Pong! ' + Math.round(client.ping) + ' ms')
+    
+  }
+  
+})
+
+client.login(process.env.TOKEN); //Never show your token bruh
