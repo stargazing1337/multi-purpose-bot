4c59b4a474f32dbc79a20b4beb99676831008d56
Glitch (hello-express) <none>
2019-02-12 00:23:46 +0000

    👘🍰 Checkpoint ./server.js:3080416/10

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ client.on('message', (message) => { // When message is recieved
   
   if(command === "ping") { // Command ping
     var pong = 'Pong! ' + Math.round(client.ping) + 'ms'
-    const embed = new Discord.RichEmbed().setTitle('Pong!').addField(pong)
+    const embed = new Discord.RichEmbed().setTitle('Pong!').addField(args, [pong])
     message.channel.send({embed: embed})
     //message.channel.send('Pong! ' + Math.round(client.ping) + 'ms')
     
