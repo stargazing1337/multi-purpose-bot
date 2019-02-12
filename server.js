f72ff62b49b623e70febd5a9a11ff9022eed8543
Glitch (discord-modbot) <none>
2019-02-12 19:19:27 +0000

    🎌🛩 Checkpoint ./server.js:3080416/1071

--- server.js
+++ server.js
@@ -60,5 +60,24 @@ client.on('message', (message) => { // When message is recieved
     .setTimestamp()
     message.channel.send(embed)    
   }
+  
+
+  
+  if (command === "prune"){
+    message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+
+            var messagesDeleted = messages.array().length; // number of messages deleted
+            // Logging the number of messages deleted on both the channel and console.
+            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+          })
+          .catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+});
+  }
+  
 })
 client.login(process.env.TOKEN); // Never show your token bruh
