414150232b0329cd1adb00fbaba78194e28e03d9
Glitch (discord-modbot) <none>
2019-02-14 04:35:48 +0000

    🍷🖐 Checkpoint ./commands/report.js:3080416/17 ./commands/kick.js:3080416/1008

--- commands/kick.js
+++ commands/kick.js
@@ -4,8 +4,23 @@ module.exports.run = async (client, message, args) => {
   
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
   if(!kUser) return message.channel.send("Can't find user!");
-  let kReason = args.join(" ").slice(22)
+  let kReason = args.join(" ").slice(22);
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have sufficient priveliges.");
+  if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("User cannot be kicked.")
   
+  let kickeEmber = new Discord.RicheEmbed()
+  .setDescription("Kick")
+  .setColor("#e56b00")
+  .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+  .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+  .addField("Kicked in", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Reason", kReason)
+  
+  let kickChannel = message.guild.channels.find(`name`, "incidents");
+  if (!kickChannel) return message.channel.send("Can't find incidents channel.");
+  
+  mes
   
 }
 
