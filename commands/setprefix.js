518b06ecefb774e2e5c74e78022db902048fae5b
Glitch (discord-modbot) <none>
2019-02-14 06:14:50 +0000

    👨‍👨‍👦🐫 Checkpoint ./commands/setprefix.js:3080416/472 ./prefixes.json:3080416/6

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const fs = require("fs");
 
 module.exports.run = async (client, message, args, prefix) => {
-  if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("NO");
+  if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("You do not have sufficient priveliges.");
   if (!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`)
 
   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
@@ -20,6 +20,8 @@ module.exports.run = async (client, message, args, prefix) => {
   .setTitle("Prefix Set!")
   .setDescription(`Set to ${args[0]}`)
   message.channel.send(sEmbed);
+  
+
 }
 
 module.exports.help = {
