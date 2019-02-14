772b7b7b98576f8830448b87a177cdb18a474ad9
Glitch (discord-modbot) <none>
2019-02-14 04:15:01 +0000

    💼💼 Checkpoint ./commands/report.js:3080416/67

--- commands/report.js
+++ commands/report.js
@@ -1,21 +1,28 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
- let rUser = message.guild.member(message.mention.users.first() || message.guild.members.get(args[0]))
+ let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
  if (!rUser) return message.channel.send("Couldn't find user.");
  let reason = args.join(" ").slice(22);
   
   let reportEmbed = new Discord.RichEmbed()
   .setDescription("Reports")
   .setColor(0x7289DA)
-  .addField("Reported User", `${rUser} with ID: $(rUser.id)`)
+  .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
   .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-  .addField ("Channel", )
-  message.channel.send(reportEmbed);
+  .addField ("Channel", message.channel)
+  .addField ("Time", message.createdAt)
+  .addField ("Reason", reason)
+
+  let reportsChannel = message.guild.channels.find(`name`, "reports")
+  if (!reportsChannel) return message.channel.send("Couldn't find the reports channel.")
   
+  message.delete().catch(O_o=>{});
+  reportsChannel.send(reportEmbed);
+  //message.channel.send(reportEmbed)  
   return
 }
 
 module.exports.help = {
-  name: ""
+  name: "report"
 }
