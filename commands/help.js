acc857f5fca273f014248fb8cc602fcf4934ae1f
Glitch (discord-modbot) <none>
2019-02-15 20:24:26 +0000

    👶🍴 Checkpoint ./commands/serverinfo.js:3080416/784 ./commands/help.js:3080416/33

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
     .setTitle(`Help Menu`)
     .addField(`Information Commands`, "`help` `about` `ping`")
     .addField(`Moderation Commands`,"`ban` `kick` `prune` `setprefix` `tempmute`")
-    .addField(`User Commands`,"`report` `kick`")
+    .addField(`User Commands`,"`report` `serverinfo` `userinfo`")
     .addField(`Fun Commands`,"`8ball` `coinflip`")
     .setFooter("Prefix: ! | This bot is still under construction")
     .setTimestamp()
