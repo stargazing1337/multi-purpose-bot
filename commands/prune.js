31e6193fa3cde604455c9ad0d237632e83eaee20
Glitch (discord-modbot) <none>
2019-02-15 04:03:13 +0000

    🎙🗼 Checkpoint ./commands/prune.js:3080416/225

--- commands/prune.js
+++ commands/prune.js
@@ -3,10 +3,10 @@ const {RichEmbed} = require("discord.js")
 module.exports.run = async (client, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have sufficient priveliges.");
   if (!args[0] || args[0 == "help"]) return message.reply(`Usage: !prune <Desired amount>`)
+  message.delete().catch(O_o=>{});
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(Cleared ${args})
-                                            })
-  
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });  
 }
 
 module.exports.help = {
