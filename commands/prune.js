2d89aec57db13d894177391e84e1fc520db08136
Glitch (discord-modbot) <none>
2019-02-15 06:24:58 +0000

    💳💷 Checkpoint ./commands/prune.js:3080416/228

--- commands/prune.js
+++ commands/prune.js
@@ -1,24 +1,20 @@
 module.exports.run = async (client, message, args, con) => {
 	//if(!client.hasPermission(message, "MANAGE_MESSAGES")) return message.channel.send("You do not have permission to do this.");
-
 	let mention = message.mentions.users.first();
-
 	let amount;
 	if(mention) {
 		amount = 100
 	} else {
 		amount = parseInt(args[0]);
-		if(!amount || amount < 1 && amount < 100) return message.channel.send("Please enter a number between 1 and 100.");
+		if(!amount || amount < 1 && amount < 100) return message.channel.send("Usage: !prune <# between 1 and 100>");
 		amount = Math.min(amount + 1, 100);
 	}
 
 	try {
 		let messages = await message.channel.fetchMessages({limit: amount});
 		messages = messages.filter(m => m.createdTimestamp >= Date.now() - 1179360000);
-		
 		let mention = message.mentions.users.first();
 		if(mention) messages = messages.filter(m => m.author.id === mention.id || m.content === message.content);
-
 		let pruned = messages.size;
 		if(pruned < 1) return message.channel.send("No prune-able messages were found.");
 		await message.channel.bulkDelete(messages);
@@ -32,5 +28,3 @@ module.exports.run = async (client, message, args, con) => {
 module.exports.help = {
 	name: "prune"
 }
-
-//:wastebasket:  |  Successfully deleted ${args[0]} messages from this channel!
