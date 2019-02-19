9f31c0e660939f263033cffbf78dac01a9e9f98c
Glitch (discord-modbot) <none>
2019-02-19 05:17:13 +0000

    😜😻 Checkpoint ./commands/say.js:3080416/401

--- commands/say.js
+++ commands/say.js
@@ -4,15 +4,23 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (client, message, args) => {
 
   if(!args [0] || args[0] == "help"){
-   message.reply("Usage: !say <word or sentence>");
+   message.reply("Usage: !say <#channel> <word or sentence>");
    return;
   }
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  
+  
   message.delete();
-  let botmessage = args.join(" ");
-  message.channel.send(botmessage);
+  let botmessage;
+  let channel = message.mentions.channels.first()
+  if(channel) {
+    botmessage = args.slice(1).join("")
+    channel.send(botmessage)
+  } else {
+   botmessage = args.join(" ");
+   message.channel.send(botmessage)
+  }
 }
-
 module.exports.help = {
   name: "say",
   help: ""
