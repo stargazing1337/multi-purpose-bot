68c4842677af707c6a41d38ce100bf3215fd0445
Glitch (discord-modbot) <none>
2019-02-20 05:15:35 +0000

    😾🚥 Checkpoint ./commands/ping.js:3080416/964

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,9 @@ module.exports.run = async (client, message, args) => {
     .setColor("#7289DA")
     .addField(`Pong!`, pong)
     message.channel.send(embed)  
+  
+      const m = await message.channel.send("Ping?");
+m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
 
 module.exports.help = {
