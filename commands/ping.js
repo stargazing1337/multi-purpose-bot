9ebdd960231c51d5aa7c1e9773a220d23e52ec4c
Glitch (discord-modbot) <none>
2019-02-20 05:48:21 +0000

    🎤👕 Checkpoint ./commands/ping.js:3080416/1690

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,11 @@
 const {RichEmbed} = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-    const m = await message.channel.send("Ping?");
-    const pong = await 
-    //var pong = `Latency is ${m.createdTimestamp - message.createdTimestamp}ms`
-    pong.edit(`doo`);
-    var api = `API Latency is ${Math.round(client.ping)}ms`
+    var pong = `It took ` + Math.round(client.ping) + `ms to ping.`
     const embed = new RichEmbed()
     .setColor("#7289DA")
-    .addField(`Pong!`, `${pong} | ${api}`)
-    message.channel.send(embed)
-  
-  
-    //m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    .addField(`Pong!`, pong)
+    message.channel.send(embed)    
 }
 
 module.exports.help = {
