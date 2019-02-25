9616f3bebe6877856961fea5fbec86260f83e184
Glitch (discord-modbot) <none>
2019-02-25 20:41:23 +0000

    👖👨‍❤️‍💋‍👨 Checkpoint ./commands/coolfolder/Test.js:3080416/396 ./index.js:3080416/521

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   console.log(`Loading a total of ${files.length} commands.`);
   files.forEach(file => {
-    //if (!file.endsWith(".js")) return;
+        console.log(`${fs.lstatSync(`./commands/${file}`).isDirectory()}`)
+    if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
-    
-    console.log(`Loading Command: ${props.help.name} ✔`);
+    //console.log(`Loading Command: ${props.help.name} ✔`);
+
     client.commands.set(props.help.name, props);
         props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
