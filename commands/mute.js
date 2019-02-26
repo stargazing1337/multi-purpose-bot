6d0fe5d63018c32dd474733693d81b8ff9d8b127
Glitch (discord-modbot) <none>
2019-02-26 03:46:24 +0000

    🛩💺 Checkpoint ./commands/mute.js:3080416/5724

--- commands/mute.js
+++ commands/mute.js
@@ -1,20 +1,48 @@
-const randomPuppy = require('random-puppy');
-const request = require('snekfetch');
-const fs = require("fs")
+const Discord = require("discord.js");
+const ms = require("ms");
 
-exports.run = (client, message, args) => {
-  //insert code here
+exports.run = async (client, message, args) => {
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute) return message.reply("Usage: !tomute <@user> <#s/h/d/m>");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  let muterole = message.guild.roles.find(`name`, "Muted");
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "Muted",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("You didn't specify a time!");
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+  }, ms(mutetime));
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['t', 'temp'],
+    aliases: ['tm'],
     permLevel: 0
   };
   
   exports.help = {
-    name: 'template',
-    description: 'Template for commands.',
-    usage: '!template'
+    name: 'tempmute',
+    description: 'Gives you a random response to a question.',
+    usage: '8ball [question]'
 };
