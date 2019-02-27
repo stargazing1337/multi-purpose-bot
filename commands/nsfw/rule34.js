8f7558b5866d0e80202ac958e3a4627afed19d71
Glitch (discord-modbot) <none>
2019-02-27 03:38:11 +0000

    🚲🌝 Checkpoint ./commands/nsfw/rule34.js:3080416/4469

--- commands/nsfw/rule34.js
+++ commands/nsfw/rule34.js
@@ -3,55 +3,41 @@ const xml2js = require('xml2js');
 
 exports.run = (client, message, args) => {
     if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
-    try {
-            // Currently there is something wrong with Commando nsfw detection... So better make sure this works
-            if(message.message.channel.nsfw){
-                if(args[0] === undefined){
-                    var argR = "";
+    let argR = args.slice(0).join(" ")
+    var url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + argR;
+
+    https.get(url, function(res) {
+        var body = '';
+
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function() {
+            var parser = new xml2js.Parser();
+            parser.parseString(body, function(err, result) {
+                var postCount = result.posts.$.count - 1;
+                if (postCount > 100) {
+                    postCount = 100;
+                }
+                if (postCount > 0) {
+                    var picNum = Math.floor(Math.random() * postCount) + 0;
+                    var r34Pic = result.posts.post[picNum].$.file_url;
+                    // console.log(result.posts.post[picNum].$.file_url);
+                    message.channel.send({
+                        files: [r34Pic]
+                    });
+
                 } else {
-                    var argR = args;
+                    console.log("Nothing found:", argR);
+                    message.channel.send("Nobody here but us chickens!");
                 }
 
-                var url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + argR;
-
-                https.get(url, function(res){
-                    var body = '';
-            
-                    res.on('data', function(chunk){
-                        body += chunk;
-                    });
-            
-                    res.on('end', function(){
-                        var parser = new xml2js.Parser();
-                        parser.parseString(body, function (err, result) {
-                            var postCount = result.posts.$.count - 1;
-                            if(postCount > 100) {
-                                postCount = 100;
-                            }
-                            if(postCount > 0) {
-                                var picNum = Math.floor(Math.random() * postCount) + 0;
-                                var r34Pic = result.posts.post[picNum].$.file_url;
-                                // console.log(result.posts.post[picNum].$.file_url);
-                                message.channel.send({
-                                    files: [r34Pic]
-                                });
-                            
-                            } else {
-                                console.log("Nothing found:", argR);
-                                message.channel.send("Nobody here but us chickens!");
-                            }
-
-                            });
-                        });
-                    }).on('error', function(e){
-                        console.log("Got an error: ", e);
-                });
-            } else {
-                message.channel.send(":warning: This channel is not NSFW!");
-            }
-        } catch(e) {
-            console.log(e);
-        }
+            });
+        });
+    }).on('error', function(e) {
+        console.log("Got an error: ", e);
+    });
 }
 
 
@@ -60,9 +46,9 @@ exports.conf = {
     guildOnly: false,
     aliases: [],
     permLevel: 0
-  };
-  
-  exports.help = {
+};
+
+exports.help = {
     name: 'rule34',
     description: 'Gives you a random response to a question.',
     usage: '8ball [question]'
