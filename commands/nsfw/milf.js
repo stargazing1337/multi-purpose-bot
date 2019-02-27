14659d851dc3ba14b0893a24d6f387307607311a
Glitch (discord-modbot) <none>
2019-02-27 01:21:11 +0000

    👀💩 Checkpoint ./commands/info/help.js:3080416/87 ./commands/nsfw/gif.js:3080416/1119 ./commands/nsfw/hentai.js:3080416/1115 ./commands/nsfw/milf.js:3080416/971 ./commands/nsfw/penis.js:3080416/924 ./commands/nsfw/pornhub.js:3080416/1009

---
+++ commands/nsfw/milf.js
@@ -0,0 +1,36 @@
+const randomPuppy = require('random-puppy');
+const request = require('snekfetch');
+const fs = require("fs")
+
+exports.run = (client, message, args) => {
+    if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+
+    var subreddits = [
+        'milf',
+        'amateur_milfs',
+        'NotTeenNotMilf'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            request.get(url).then(r => {
+                fs.writeFile(`milf.jpg`, r.body)
+                message.channel.sendFile(r.body)
+                fs.unlink(`./milf.jpg`)
+            })
+        })
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'milf',
+    description: 'Gives you a random response to a question.',
+    usage: '8ball [question]'
+};
