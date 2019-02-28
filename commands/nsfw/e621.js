46afaa23076090d3388046336e0ffa37a25c4d1b
Glitch (discord-modbot) <none>
2019-02-28 00:54:04 +0000

    🍹🍉 Checkpoint ./package.json:3080416/1859 ./commands/nsfw/e621.js:3080416/13244

--- commands/nsfw/e621.js
+++ commands/nsfw/e621.js
@@ -1,41 +1,32 @@
-const https = require("https");
+const Discord = require("discord.js")
 const xml2js = require('xml2js');
+const booru = require('booru');
 
 exports.run = (client, message, args) => {
     if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
-    let argR = args.slice(0).join(" ")
-    var url = 'https://e621.net/post/index?tags=' + argR;
 
-    https.get(url, function(res) {
-        var body = '';
+        if (message.content.toUpperCase().includes('LOLI') || message.content.toUpperCase().includes('GORE')) return message.channel.send('That kind of stuff is not allowed! Not even in NSFW channels!');
 
-        res.on('data', function(chunk) {
-            body += chunk;
-        });
+        var query = message.content.split(/\s+/g).slice(1).join(" ");
 
-        res.on('end', function() {
-            var parser = new xml2js.Parser();
-            parser.parseString(body, function(err, result) {
-                var postCount = result.posts.$.count - 1;
-                if (postCount > 100) {
-                    postCount = 100;
+        booru.search('e621', [query], { limit: 1, random: true })
+            .then(booru.commonfy)
+            .then(images => {
+                for (let image of images) {
+                    const embed = new Discord.MessageEmbed()
+                        .setAuthor(`e621 ${query}`, 'https://b.catgirlsare.sexy/NrAI.png')
+                        .setDescription(`[Image URL](${image.common.file_url})`)
+                        .setImage(image.common.file_url)
+                        .setColor('#E89F3E');
+                    return message.channel.send({ embed });
                 }
-                if (postCount > 0) {
-                    var picNum = Math.floor(Math.random() * postCount) + 0;
-                    var r34Pic = result.posts.post[picNum].$.file_url;
-                    // console.log(result.posts.post[picNum].$.file_url);
-                    message.channel.send({
-                        files: [r34Pic]
-                    });
+            }).catch(err => {
+                if (err.name === 'booruError') {
+                    return message.channel.send(`No results found for **${query}**!`);
                 } else {
-                    console.log("Nothing found:", argR);
-                    message.channel.send("No images found.");
+                    return message.channel.send(`No results found for **${query}**!`);
                 }
-            });
-        });
-    }).on('error', function(e) {
-        console.log("Got an error: ", e);
-    });
+            })
 }
 
 exports.conf = {
