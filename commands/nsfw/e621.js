98806656397a892cd27b85fb7e518e0e0d0a0892
Glitch (discord-modbot) <none>
2019-02-28 01:05:04 +0000

    🏂⛵️ Checkpoint ./commands/nsfw/e621.js:3080416/2595

--- commands/nsfw/e621.js
+++ commands/nsfw/e621.js
@@ -1,32 +1,34 @@
 const Discord = require("discord.js")
-const xml2js = require('xml2js');
 const booru = require('booru');
 
 exports.run = (client, message, args) => {
-    if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
 
-        if (message.content.toUpperCase().includes('LOLI') || message.content.toUpperCase().includes('GORE')) return message.channel.send('That kind of stuff is not allowed! Not even in NSFW channels!');
+var query = args.slice(0).join(" ");
 
-        var query = message.content.split(/\s+/g).slice(1).join(" ");
-
-        booru.search('e621', [query], { limit: 1, random: true })
-            .then(booru.commonfy)
-            .then(images => {
-                for (let image of images) {
-                    const embed = new Discord.MessageEmbed()
-                        .setAuthor(`e621 ${query}`, 'https://b.catgirlsare.sexy/NrAI.png')
-                        .setDescription(`[Image URL](${image.common.file_url})`)
-                        .setImage(image.common.file_url)
-                        .setColor('#E89F3E');
-                    return message.channel.send({ embed });
-                }
-            }).catch(err => {
-                if (err.name === 'booruError') {
-                    return message.channel.send(`No results found for **${query}**!`);
-                } else {
-                    return message.channel.send(`No results found for **${query}**!`);
-                }
-            })
+booru.search('safebooru', [query], {
+        limit: 1,
+        random: true
+    })
+    .then(booru.commonfy)
+    .then(images => {
+        for (let image of images) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`safebooru ${query}`)
+                .setDescription(`[Image URL](${image.common.file_url})`)
+                .setImage(image.common.file_url)
+                .setColor('#E89F3E');
+            return message.channel.send({
+                embed
+            });
+        }
+    }).catch(err => {
+        if (err.name === 'booruError') {
+            return message.channel.send(`No results found for **${query}**!`);
+        } else {
+            return message.channel.send(`No results found for **${query}**!`);
+        }
+    })
 }
 
 exports.conf = {
