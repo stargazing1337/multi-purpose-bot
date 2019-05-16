ad3b98a6fd4134a5a0aa670066183e7671f7935a
Glitch (discord-modbot) <none>
2019-05-16 05:06:35 +0000

    💐🏈 Checkpoint ./commands/mod/prefix.js:3080416/652

--- commands/mod/prefix.js
+++ commands/mod/prefix.js
@@ -6,11 +6,11 @@ exports.run = async (client, message, args, prefix) => {
   if (!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`)
   //let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"))
   
-  prefixes [message.guild.id] = {
-  prefixes: args.slice(0).join(" ")
+ prefix[message.guild.id] = {
+  prefix: args.slice(0).join(" ")
   };
   
-  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) =>{
+  fs.writeFile("./prefixes.json", JSON.stringify(prefix), (err) =>{
   if (err) console.log(err)
   });
   
