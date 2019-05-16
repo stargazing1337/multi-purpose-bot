1ce772b844670cba1fcc40496bab4e021a34aa87
Glitch (discord-modbot) <none>
2019-05-16 04:55:35 +0000

    🚍💪 Checkpoint ./commands/mod/prefix.js:3080416/2

--- commands/mod/prefix.js
+++ commands/mod/prefix.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 exports.run = async (client, message, args, prefix) => {
   if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("You do not have sufficient priveliges.");
   if (!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`)
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"))
+  //let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"))
   
   prefixes [message.guild.id] = {
   prefixes: args.slice(0).join(" ")
